f80106765908eae31ebe3eae093a6afc957a8d4c
BADAVATH SRIKANTH <80474350+SRIKANTH284@users.noreply.github.com>
2023-04-21 23:57:28 +0530

    Add files via upload

---
+++ src/test/java/com/javatechie/DevopsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sureshrajuvetukuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
